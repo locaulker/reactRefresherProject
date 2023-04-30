c39238fba7024bedb1f9b4f32db7696130e45fb3
Lawrence Caulker <locaulker@gmail.com>
2023-04-30 07:09:46 -0400

    initial setup

---
+++ src/components/GoalList.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import "./GoalList.css"
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish the Course</li>
+      <li>Lear all about the main Topics of the Course</li>
+      <li>Help other students in the Q&A</li>
+    </ul>
+  )
+}
+
+export default GoalList
